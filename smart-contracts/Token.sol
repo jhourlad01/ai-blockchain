167820f8d9d6940e04f3220bdf5b444df6e3d165
Joe Estrella <125230604+jhourlad01@users.noreply.github.com>
2025-05-22 09:00:24 +0800

    Update Token.sol

--- smart-contracts/Token.sol
+++ smart-contracts/Token.sol
@@ -5,3 +5,46 @@ pragma solidity ^0.8.0;
  * @title SimpleToken
  * @dev A basic ERC20-like token contract for demonstration purposes.
  */
+contract SimpleToken {
+    string public name = "SimpleToken";
+    string public symbol = "SIM";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _amount) external returns (bool) {
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) external returns (bool) {
+        require(balanceOf[_from] >= _amount, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _amount, "Allowance exceeded");
+
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+        allowance[_from][msg.sender] -= _amount;
+
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+}
